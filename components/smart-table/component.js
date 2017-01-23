187915e436dfe4e6ca597b413a1fe05d4285bb57
Sinha0542 <geeksinha952@gmail.com>
2017-01-23 06:32:12 +0000

    navbar in one tab

--- components/smart-table/component.js
+++ components/smart-table/component.js
@@ -31,32 +31,44 @@ function customTableCtrl() {
 // searchopentab is a function that will hide the main toolbar
     customTableCtrl.serchopentab = function() {
         customTableCtrl.options.Search = true;
-        customTableCtrl.options.rowSelection = false;
+        customTableCtrl.options.rowSelection=true;
+       
+    }
+    customTableCtrl.closeSearchnavbar=function()
+    {
+          customTableCtrl.options.Search = false;
+        customTableCtrl.options.rowSelection=false;
     }
     customTableCtrl.options = {
         rowSelection: false,
         pageSelect: true,
         Search: false,
-        deletenavbar:true,
+        deletenavbar:false
+
     };
 
     customTableCtrl.selected = [];
 
     customTableCtrl.query = {
-        order: 'name',
         limit: 5,
         page: 1
     };
 // tablerow is a function passing the respective id of a particular row 
     customTableCtrl.tablerow = function(tableobj) {
-        customTableCtrl.deletenavbar=true;
+        customTableCtrl.options.deletenavbar=true;
         customTableCtrl.options.rowSelection = true;
+      
+       
         console.log(tableobj._id);
         customTableCtrl.id = tableobj._id;
     }
  //   seclectedRowId is a function passing the respective id of a particular row 
     customTableCtrl.seclectedRowId = function() {
-
+         
+      customTableCtrl.options.rowSelection=false;
+      
+      customTableCtrl.options.deletenavbar=false;
+      
         customTableCtrl.tablerowid({ 'id': customTableCtrl.id });
     }
 
