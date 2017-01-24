217d8b3435a578bf8a378e4ea50ece374a666647
RamanK17 <ramandeep1794@gmail.com>
2017-01-24 13:26:29 +0000

    Sorting

--- components/smart-table/component.js
+++ components/smart-table/component.js
@@ -10,7 +10,8 @@ angular.module('ngTable').component('customTable', {
         pagecount: '@',
         searchinput: '&',
         tablerowid: '&',
-        tableheader: '&'
+        tableheader: '&',
+        sortflags: '<'
 
     },
     controller: customTableCtrl,
@@ -23,12 +24,41 @@ function customTableCtrl() {
 
     var customTableCtrl = this;
     console.log(this);
-// headervalue is a function which passed the respective header 
-    customTableCtrl.headervalue = function(header) {
 
-        customTableCtrl.tableheader({ 'key': header });// calling the parent tableheader() function by passing the header of table 
+    customTableCtrl.sortFields=function(){
+        console.log("sortField function is called",customTableCtrl.sortflags);
     }
-// searchopentab is a function that will hide the main toolbar
+    // headervalue is a function which passed the respective header 
+    customTableCtrl.headervalue = function(header) {
+            var sortOrder;
+            if(customTableCtrl.sortflags[header].ascending===false && customTableCtrl.sortflags[header].descending===false)
+            {
+                customTableCtrl.sortflags[header].ascending=true;
+                sortOrder="ascending";
+            }
+            else {
+
+                customTableCtrl.sortflags[header].ascending=!customTableCtrl.sortflags[header].ascending;
+                customTableCtrl.sortflags[header].descending=!customTableCtrl.sortflags[header].descending;
+                if(customTableCtrl.sortflags[header].ascending === true){
+                    sortOrder="ascending";
+                }
+                else{
+                    sortOrder="descending";
+                }
+            }
+            if(customTableCtrl.currentSortHeader !== undefined && customTableCtrl.currentSortHeader !== header ){
+                customTableCtrl.sortflags[customTableCtrl.currentSortHeader].ascending=false;
+                customTableCtrl.sortflags[customTableCtrl.currentSortHeader].descending=false;
+
+            }
+            customTableCtrl.currentSortHeader=header;
+
+            customTableCtrl.tableheader({ 'key': header, 'sortOrder': sortOrder}); // calling the parent tableheader() function by passing the header of table 
+
+
+        }
+        // searchopentab is a function that will hide the main toolbar
     customTableCtrl.serchopentab = function() {
         customTableCtrl.options.Search = true;
         customTableCtrl.options.rowSelection = false;
@@ -37,7 +67,7 @@ function customTableCtrl() {
         rowSelection: false,
         pageSelect: true,
         Search: false,
-        deletenavbar:true,
+        deletenavbar: true,
     };
 
     customTableCtrl.selected = [];
@@ -47,27 +77,25 @@ function customTableCtrl() {
         limit: 5,
         page: 1
     };
-// tablerow is a function passing the respective id of a particular row 
+    // tablerow is a function passing the respective id of a particular row 
     customTableCtrl.tablerow = function(tableobj) {
-        customTableCtrl.deletenavbar=true;
-        customTableCtrl.options.rowSelection = true;
-        console.log(tableobj._id);
-        customTableCtrl.id = tableobj._id;
-    }
- //   seclectedRowId is a function passing the respective id of a particular row 
+            customTableCtrl.deletenavbar = true;
+            customTableCtrl.options.rowSelection = true;
+            console.log(tableobj._id);
+            customTableCtrl.id = tableobj._id;
+        }
+        //   seclectedRowId is a function passing the respective id of a particular row 
     customTableCtrl.seclectedRowId = function() {
 
         customTableCtrl.tablerowid({ 'id': customTableCtrl.id });
     }
 
-//tableinformation is user for first time loading the table data
+    //tableinformation is user for first time loading the table data
     customTableCtrl.tableinformation({ "pageno": 0, 'pagelimit': 5 });
     customTableCtrl.onPaginate = function(pageno, pagelimit) {
-//onPaginate is a function that iterate on the no of pages as well as calling the parent tableinformation function  
+        //onPaginate is a function that iterate on the no of pages as well as calling the parent tableinformation function  
         customTableCtrl.tableinformation({ "pageno": pageno, 'pagelimit': pagelimit });
 
     }
-
-
 
 }
--- module.js
+++ module.js
@@ -2,29 +2,47 @@ angular.module("ngTable", ['ngMaterial', 'md.data.table'])
     .controller('ngTableCtrl', function(tableDataService) {
 
         var ngTablectrl = this;
-//table information is a function which will fetch table data from service tableDataService
+        ngTablectrl.fieldSortFlags = {}
+        ngTablectrl.callFunc = function() {
+                console.log("callFunction", ngTablectrl.tabledata);
+                var object = ngTablectrl.tabledata[0];
+                console.log(object);
+                for (var key in object) {
+
+                    ngTablectrl.fieldSortFlags[key] = { "ascending": false, "descending": false }
+
+                }
+                console.log("Inside parent ctrl CallFUnction",ngTablectrl.fieldSortFlags);
+
+            }
+            //table information is a function which will fetch table data from service tableDataService
         ngTablectrl.tableinformation = function(pageno, pagelimit) {
-            console.log("pageno", pageno, "limitttt", pagelimit);
-            var promise = tableDataService.servicedata(pageno, pagelimit);
-            promise.then(function(userobj) {
-                ngTablectrl.pageCount = userobj.pageCount;
-                console.log("total count", ngTablectrl.pageCount);
-                ngTablectrl.tabledata = userobj.data;
-                console.log(ngTablectrl.tabledata);
-            });
+                console.log("pageno", pageno, "limitttt", pagelimit);
+                var promise = tableDataService.servicedata(pageno, pagelimit);
+                promise.then(function(userobj) {
+                    ngTablectrl.pageCount = userobj.pageCount;
+                    console.log("total count", ngTablectrl.pageCount);
+                    ngTablectrl.tabledata = userobj.data;
+                    ngTablectrl.callFunc();
+                    console.log("Inside Parent Ctrl tableinformation function",ngTablectrl.tabledata);
+                });
+
+            }
+            // searchinput is a function called from the child component which will show searched text
+        ngTablectrl.searchinput = function(text) {
+                console.log("search text", text);
+            }
+            //tablerowid is a function called from the child component which will get the selected row id        
+        ngTablectrl.tablerowid = function(id) {
+                console.log("id deleted user", id);
+            }
+            //tableheader is a function called from the child component which  will get the header value of the selected column in table.
+        ngTablectrl.tableheader = function(key,sortOrder) {
+            console.log("in parentctrl header", key);
+            console.log("in parentctrl sortOrder", sortOrder);
+
 
         }
-// searchinput is a function called from the child component which will show searched text
-        ngTablectrl.searchinput = function(text) {
-            console.log("search text", text);
-        }
-//tablerowid is a function called from the child component which will get the selected row id        
-        ngTablectrl.tablerowid = function(id) {
-            console.log("id deleted user", id);
-        }
-//tableheader is a function called from the child component which  will get the header value of the selected column in table.
-        ngTablectrl.tableheader = function(key) {
-            console.log("in parentctrl header", key);
-        }
+
 
     });
